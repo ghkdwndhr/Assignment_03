c53dfc034c0c1af4b6f68d3e2c8565d18f26e3de
ghkdwndhr <ghkdwndhr227@naver.com>
2025-03-25 17:30:22 +0900

    Initial commit

--- examples/myworld/sketch.js
+++ examples/myworld/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(600, 400);
-  background('pink');
+  background('lightgray');
   textSize(32);
   textAlign(CENTER, CENTER);
   text("Hello World!", width/2, height/2);
